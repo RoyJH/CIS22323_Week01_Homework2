ffe0a169d6e23474ddf2238ef403dee3447d3a3e
RoyJH <royjh@okstate.edu>
2018-08-29 11:26:29 -0500

    Initial commit

---
+++ CardGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class CardGUI {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "<========--------OSU_OKC_CIS:2323--------========>\n" +
+		 "| Roy J. Harris III 'Trey'                                                           |\n" +
+		 "| royjh@okstate.edu                                                              |\n" +
+		 "| 1006 W Ridgecrest Way,                   github.com/royjh |\n" +
+		 "| Mustang, OK 73064	                      Phone:405.613.2591 |\n" +
+		 "|                                                                                                   |\n" +
+		 "<========--------Java_Programming--------========>");
+	}
+}
